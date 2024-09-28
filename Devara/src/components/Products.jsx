c0597767250b958834bccabdcdbda0f1336f192d
orrino213 <orrinohanian13@gmail.com>
2024-09-28 15:31:56 -0700

    a

---
+++ Devara/src/components/Products.jsx
@@ -0,0 +1,59 @@
+import iphoneSE from "../assets/products/iphoneSE.webp";
+import gamingChair from "../assets/products/gamingChair.jpg";
+import headphones from "../assets/products/headphones.jpg";
+import hoodie from "../assets/products/hoodie.jpg";
+import macbook from "../assets/products/macbook.jpg";
+import mouse from "../assets/products/mouse.avif";
+import tshirt from "../assets/products/tshirt.jpg";
+import waterBottle from "../assets/products/waterBottle.webp";
+
+export const products = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: iphoneSE,
+  },
+  {
+    id: 2,
+    productName: "Gaming Chair",
+    price: 49.99,
+    productImage: gamingChair,
+  },
+  {
+    id: 3,
+    productName: "headphones",
+    price: 69.99,
+    productImage: headphones,
+  },
+  {
+    id: 4,
+    productName: "Hoodie",
+    price: 49.99,
+    productImage: hoodie,
+  },
+  {
+    id: 5,
+    productName: "Macbook",
+    price: 1199.0,
+    productImage: macbook,
+  },
+  {
+    id: 6,
+    productName: "Mouse",
+    price: 29.99,
+    productImage: mouse,
+  },
+  {
+    id: 7,
+    productName: "T-shirt",
+    price: 24.99,
+    productImage: tshirt,
+  },
+  {
+    id: 8,
+    productName: "Water Bottle",
+    price: 29.99,
+    productImage: waterBottle,
+  },
+];
--- Devara/src/components/Shop.jsx
+++ Devara/src/components/Shop.jsx
@@ -1,10 +1,18 @@
+import { products } from "./products";
+import { Product } from "./Product";
+
 function Shop() {
   return (
     <div className="shop">
       <div className="shop-title">
         <h1>Devara Shop</h1>
       </div>
-      <div className="products"></div>
+      <div className="products">
+        {""}
+        {products.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
     </div>
   );
 }
