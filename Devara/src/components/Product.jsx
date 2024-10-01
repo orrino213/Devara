a1020043f645b15b80bb01aeed9e738728ec8384
orrino213 <orrinohanian13@gmail.com>
2024-10-01 15:26:10 -0700

    a

--- Devara/src/components/Product.jsx
+++ Devara/src/components/Product.jsx
@@ -1,4 +1,13 @@
-export const product = (props) => {
-  const { id, productNAme, price, productImage } = props.data;
-  return <div>{productName}</div>;
+export const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  return (
+    <div className="product">
+      <img src={productImage} />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+      </div>
+    </div>
+  );
 };
